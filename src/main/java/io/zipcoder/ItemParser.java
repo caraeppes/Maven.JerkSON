59443d9f70b92b31218fe9ac85fc4ea3d8def6e4
Cara Eppes <ceppes@udel.edu>
2019-03-18 14:46:53 -0400

    abstracted find methods into one find method

--- src/main/java/io/zipcoder/ItemParser.java
+++ src/main/java/io/zipcoder/ItemParser.java
@@ -5,22 +5,27 @@ import io.zipcoder.utils.ItemParseException;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.TreeMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ItemParser{
 
-    private int exceptionCount = 0;
+    private int exceptionCount;
+    private Pattern namePattern, pricePattern, typePattern, expirationPattern;
+
+    public ItemParser() {
+        exceptionCount = 0;
+        namePattern = Pattern.compile("[N|n][A|a][M|m][E|e][@|^|*|:|%]([a-zA-Z0]+);");
+        pricePattern = Pattern.compile("[P|p][R|r][I|i][C|c][E|e][@|^|*|:|%](\\d+\\.\\d+);");
+        typePattern = Pattern.compile("type[@|^|*|:|%]([A-Za-z]+)[;|/^|/!|%|*|@]");
+        expirationPattern = Pattern.compile("expiration[@|^|*|:|%](\\d+/\\d+/\\d{4})");
+    }
 
     public List<Item> parseItemList(String valueToParse) {
-        String[] itemArray = valueToParse.split("##");
         List<Item> parsedList = new ArrayList<>();
-        for (String s : itemArray) {
+        for (String s : valueToParse.split("##")) {
             try {
-                System.out.println(s);
-                Item parsedItem = parseSingleItem(s);
-                parsedList.add(parsedItem);
+                parsedList.add(parseSingleItem(s));
             } catch (ItemParseException e){
                 System.out.println("Invalid item found.");
                 exceptionCount++;
@@ -30,60 +35,26 @@ public class ItemParser{
     }
 
     public Item parseSingleItem(String singleItem) throws ItemParseException {
-        String name = findName(singleItem);
-        Double price = findPrice(singleItem);
-        String type = findType(singleItem);
-        String expiration = findExpiration(singleItem);
-        return new Item(name, price, type, expiration);
-
+        return new Item(replace0withO(findItemField(singleItem, namePattern)),
+                new Double(findItemField(singleItem, pricePattern)),
+                findItemField(singleItem, typePattern),
+                findItemField(singleItem, expirationPattern));
     }
 
-    public String findName(String singleItem) throws ItemParseException {
-        String name = "";
-        Pattern namePattern = Pattern.compile("[N|n][A|a][M|m][E|e][@|^|*|:|%]([a-zA-Z0]+);");
-        Matcher nameMatcher = namePattern.matcher(singleItem);
-        if (nameMatcher.find()) {
-            name = nameMatcher.group(1).toLowerCase();
-            Pattern zeroPattern = Pattern.compile("0");
-            Matcher zeroMatcher = zeroPattern.matcher(name);
-            name = zeroMatcher.replaceAll("o");
-            return name;
-        }
-        throw new ItemParseException();
-
+    public Matcher getMatcher(String singleItem, Pattern pattern){
+        return pattern.matcher(singleItem);
     }
 
-    public Double findPrice(String singleItem) throws ItemParseException {
-        Double price;
-        Pattern pricePattern = Pattern.compile("[P|p][R|r][I|i][C|c][E|e][@|^|*|:|%](\\d+\\.\\d+);");
-        Matcher priceMatcher = pricePattern.matcher(singleItem);
-        if (priceMatcher.find()) {
-            price = new Double(priceMatcher.group(1));
-            return price;
+    public String findItemField(String singleItem, Pattern pattern) throws ItemParseException {
+        Matcher matcher = getMatcher(singleItem, pattern);
+        if(matcher.find()){
+            return matcher.group(1).toLowerCase();
         }
         throw new ItemParseException();
     }
 
-    public String findType(String singleItem) throws ItemParseException {
-        String type = "";
-        Pattern typePattern = Pattern.compile("type[@|^|*|:|%]([A-Za-z]+)[;|/^|/!|%|*|@]");
-        Matcher typeMatcher = typePattern.matcher(singleItem);
-        if (typeMatcher.find()) {
-            type = typeMatcher.group(1).toLowerCase();
-            return type;
-        }
-        throw new ItemParseException();
-    }
-
-    public String findExpiration(String singleItem) throws ItemParseException {
-        String expiration = "";
-        Pattern expirationPattern = Pattern.compile("expiration[@|\\^|\\*|:|%](\\d+\\/\\d+\\/\\d{4})");
-        Matcher expirationMatcher = expirationPattern.matcher(singleItem);
-        if (expirationMatcher.find()) {
-            expiration = expirationMatcher.group(1);
-            return expiration;
-        }
-        throw new ItemParseException();
+    public String replace0withO(String string){
+        return Pattern.compile("0").matcher(string).replaceAll("o");
     }
 
     public int getExceptionCount() {
