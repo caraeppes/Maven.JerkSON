81e876f55fbf28671de6c9bc4c99be2a0f7bde35
Cara Eppes <ceppes@udel.edu>
2019-03-18 11:51:30 -0400

    all item parser tests passing

--- src/main/java/io/zipcoder/GroceryReporter.java
+++ src/main/java/io/zipcoder/GroceryReporter.java
@@ -4,13 +4,19 @@ import io.zipcoder.utils.FileReader;
 
 public class GroceryReporter {
     private final String originalFileText;
+    private String[] itemArray;
 
     public GroceryReporter(String jerksonFileName) {
         this.originalFileText = FileReader.readFile(jerksonFileName);
+        itemArray = originalFileText.split("##");
+        for(String s : itemArray){
+            System.out.println(s);
+        }
     }
 
     @Override
     public String toString() {
         return null;
     }
+
 }
--- src/main/java/io/zipcoder/ItemParser.java
+++ src/main/java/io/zipcoder/ItemParser.java
@@ -3,14 +3,80 @@ package io.zipcoder;
 import io.zipcoder.utils.Item;
 import io.zipcoder.utils.ItemParseException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.TreeMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class ItemParser {
+public class ItemParser<V> {
+
     public List<Item> parseItemList(String valueToParse) {
-        return null;
+        String[] itemArray = valueToParse.split("##");
+        List<Item> parsedList = new ArrayList<>();
+        for (String s : itemArray) {
+            try {
+                Item parsedItem = parseSingleItem(s);
+                parsedList.add(parsedItem);
+            } catch (ItemParseException e){
+                System.out.println("Invalid item found.");
+            }
+        }
+        return parsedList;
     }
 
     public Item parseSingleItem(String singleItem) throws ItemParseException {
-        return null;
+        String name = findName(singleItem);
+        Double price = findPrice(singleItem);
+        String type = findType(singleItem);
+        String expiration = findExpiration(singleItem);
+        return new Item(name, price, type, expiration);
+
     }
+
+    public String findName(String singleItem) throws ItemParseException {
+        String name = "";
+        Pattern namePattern = Pattern.compile("[N|n][A|a][M|m][E|e][@|^|*|:|%]([a-zA-Z]+);");
+        Matcher nameMatcher = namePattern.matcher(singleItem);
+        if (nameMatcher.find()) {
+            name = nameMatcher.group(1).toLowerCase();
+            return name;
+        }
+        throw new ItemParseException();
+
+    }
+
+    public Double findPrice(String singleItem) throws ItemParseException {
+        Double price;
+        Pattern pricePattern = Pattern.compile("price[@|^|*|:|%](\\d+\\.\\d+);");
+        Matcher priceMatcher = pricePattern.matcher(singleItem);
+        if (priceMatcher.find()) {
+            price = new Double(priceMatcher.group(1));
+            return price;
+        }
+        throw new ItemParseException();
+    }
+
+    public String findType(String singleItem) throws ItemParseException {
+        String type = "";
+        Pattern typePattern = Pattern.compile("type[@|^|*|:|%]([A-Za-z]+);");
+        Matcher typeMatcher = typePattern.matcher(singleItem);
+        if (typeMatcher.find()) {
+            type = typeMatcher.group(1).toLowerCase();
+            return type;
+        }
+        throw new ItemParseException();
+    }
+
+    public String findExpiration(String singleItem) throws ItemParseException {
+        String expiration = "";
+        Pattern expirationPattern = Pattern.compile("expiration[@|\\^|\\*|:|%](\\d+\\/\\d+\\/\\d{4})");
+        Matcher expirationMatcher = expirationPattern.matcher(singleItem);
+        if (expirationMatcher.find()) {
+            expiration = expirationMatcher.group(1);
+            return expiration;
+        }
+        throw new ItemParseException();
+    }
+
 }
